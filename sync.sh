4417fe3542253208cd955f4572f2c0db89247a36
tuhailong <tuhailong@outlook.com>
2023-06-28 20:15:36 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,62 +1,87 @@
 #!/bin/bash
 
-acks_dir="/home/tuhailong/1_ACKs"
-declare -a acks_branches=(
-    [0]="android-mainline"
-    [1]="android14-6.1"
-    [3]="android13-5.15"
-    [5]="android12-5.10"
-    [7]="android11-5.4"
-)
-for key in ${!acks_branches[*]}; do
-    branch="common-${acks_branches[${key}]}"
-    des_dir="${acks_dir}/${key}_${acks_branches[${key}]}"
-    echo ${des_dir} ${branch}
+KNLS_ROOT=${HOME}/0_Kernel
+ACKS_ROOT=${HOME}/1_ACKs
+AOSP_ROOT=${HOME}/2_AOSP
+PIXEL_ROOT=${HOME}/3_Pixel
 
-    if [ ! -d "${des_dir}" ]; then
-        mkdir -p ${des_dir}
-        cd ${des_dir}
-        repo init -u https://android.googlesource.com/kernel/manifest -b ${branch}
+cd ${HOME}
+for dir in ${KNLS_ROOT}/*
+do
+    if test -d $dir
+    then
+        cd $dir
+        echo "Sync latest kernel code in $dir"
+        git pull
+
+        sleep 3
+
+        echo "Sync latest kernel code in $dir"
+        git pull
     fi
-
-    cd ${des_dir}
-    echo "sync code in `pwd`"
-    repo sync -c -j16
-    repo sync -c -j16
 done
-cd /home/tuhailong
 
+cd ${HOME}
+for dir in ${ACKS_ROOT}/*
+do
+    if test -d $dir
+    then
+        cd $dir
+        cd .repo/repo && git pull
+        cd $dir
+        echo "Sync latest aosp code in $dir"
+        repo sync -c -j8
 
-aosp_dir="/home/tuhailong/2_AOSP"
-declare -a aosp_branches=(
-    [0]="master"
-    [1]="android-13.0.0_r43"
-)
-for key in ${!aosp_branches[*]}; do
-    branch="${aosp_branches[${key}]}"
-    des_dir="${aosp_dir}/${key}_${aosp_branches[${key}]}"
-    echo ${des_dir} ${branch}
+        sleep 3
 
-    if [ ! -d "${des_dir}" ]; then
-        mkdir -p ${des_dir}
-        cd ${des_dir}
-        repo init -u https://android.googlesource.com/a/platform/manifest -b ${branch}
+        cd $dir
+        cd .repo/repo && git pull
+        cd $dir
+        echo "Sync latest aosp code in $dir"
+        repo sync -c -j8
     fi
-
-    cd ${des_dir}
-    echo "sync code in `pwd`"
-    repo sync -c -j16
-    repo sync -c -j16
 done
-cd /home/tuhailong
 
+cd ${HOME}
+for dir in ${AOSP_ROOT}/*
+do
+    if test -d $dir
+    then
+        cd $dir
+        cd .repo/repo && git pull
+        cd $dir
+        echo "Sync latest aosp code in $dir"
+        #repo forall -c "git reset --hard" -j8
+        repo sync -c -j8
 
-# java -Xmx32g \
-#     -Djava.util.logging.config.file=/home/tuhailong/opengrok/doc/logging.properties \
-#     -jar /home/tuhailong/opengrok/lib/opengrok.jar \
-#     -c /usr/bin/ctags \
-#     -s /home/tuhailong/opengrok/src -d /home/tuhailong/opengrok/data -H -P -S -G -v \
-#     -W /home/tuhailong/opengrok/etc/configuration.xml -U http://localhost:8080/source \
-#     --depth 51200 \
-#     --progress \
-#     -m 4096
+        sleep 3
+
+        cd $dir
+        cd .repo/repo && git pull
+        cd $dir
+        echo "Sync latest aosp code in $dir"
+        repo sync -c -j8
+    fi
+done
+
+cd ${HOME}
+for dir in ${PIXEL_ROOT}/*
+do
+    if test -d $dir
+    then
+        cd $dir
+        cd .repo/repo && git pull
+        cd $dir
+        echo "Sync latest aosp code in $dir"
+        repo sync -c -j8
+
+        sleep 3
+
+        cd $dir
+        cd .repo/repo && git pull
+        cd $dir
+        echo "Sync latest aosp code in $dir"
+        repo sync -c -j8
+    fi
+done
+cd ${HOME}
